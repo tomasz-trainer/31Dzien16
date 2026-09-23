d063a942a0a0887d2364d24fb914d7bf8336ce70
tomasz <csharp@406-00>
2026-09-23 10:33:39 +0200

    q

---
+++ P08ShopWebApp.Client/Data/ShopContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using P08ShopWebApp.Client.Models;
+
+namespace P08ShopWebApp.Client.Data;
+
+public partial class ShopContext : DbContext
+{
+    public ShopContext()
+    {
+    }
+
+    public ShopContext(DbContextOptions<ShopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=MySuperShop;Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Barcode).HasMaxLength(12);
+            entity.Property(e => e.Price).HasColumnType("decimal(8, 2)");
+            entity.Property(e => e.Title).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
