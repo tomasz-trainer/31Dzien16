683a6a99267d9995f82698af7c61159596445fd7
tomasz <csharp@406-00>
2026-09-23 11:58:50 +0200

    q

---
+++ P09ShopWebApp.Client/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P09ShopWebApp.Client.Controllers
+{
+    public class CalculatorController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(IFormCollection form)
+        {
+            //int number1 = int.Parse(form["number1"]);
+            //int number2 = int.Parse(form["number2"]);
+
+            bool ok1 = int.TryParse(form["number1"], out int number1);
+            bool ok2 = int.TryParse(form["number2"], out int number2);
+
+            if (!ok1 || !ok2)
+            {
+                ViewBag.ErrorMessage = "Invalid input. Please enter valid integers.";
+                return View("Index");
+            }
+
+            int result = number1 + number2;
+
+            ViewBag.MyResult = result;
+            ViewBag.Number1 = number1;
+            ViewBag.Number2 = number2;
+
+            ViewData["MyResult2"] = result;
+
+            return View("Index");
+        }
+    }
+}
---
+++ P09ShopWebApp.Client/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using P09ShopWebApp.Client.Models;
+using System.Diagnostics;
+
+namespace P09ShopWebApp.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
